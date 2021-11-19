6072eddfd9aba3cc96ae6e0edb1d8586b9fb1706
mmartofel <mmartofe@redhat.com>
2021-11-19 19:23:51 +0100

    add scratch of postgress deployment

---
+++ postgress/create_postgres_instance.sh
@@ -0,0 +1,7 @@
+
+oc new-app registry.redhat.io/rhel8/postgresql-12:latest --env-file=$1 --name=$2
+
+oc label deployment/$2 app.kubernetes.io/name=postgresql --overwrite
+oc label deployment/$2 app.kubernetes.io/component=database --overwrite
+oc label deployment/$2 app.kubernetes.io/part-of=ANGULAR_DEMO_APP --overwrite
+
---
+++ postgress/run.sh
@@ -0,0 +1,1 @@
+./create_postgres_instance.sh pg.env store
