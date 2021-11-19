db0fd21145b2638699f0dea449f17a0f5f956823
mmartofel <mmartofe@redhat.com>
2021-11-19 23:17:16 +0100

    scripts for OCP deployment provided

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+./mvnw clean package \
+    -Dquarkus.container-image.build=true \
+    -Dquarkus.kubernetes-client.trust-certs=true \
+    -DskipTests \
+  -Dquarkus.profile=ocp
+
+
+oc delete all --selector app=backend
+
+oc new-app --name=backend `oc project --short=true`/backend:2.0
+oc set env deployment/backend --from configmap/angular-demo-app-configmap
+oc expose service/backend
+
+oc label deployment/backend app.kubernetes.io/part-of=ANGULAR_DEMO_APP --overwrite
+oc label deployment/backend app.openshift.io/runtime=quarkus --overwrite
+
+echo
+echo "Try now to hit your backend service at:"
+echo
+echo "   http://"`oc get route backend -o template --template='{{ .spec.host }}'` 
+echo
+echo "Or first check Swagger UI to explore API provided by microservice:"
+echo
+echo "   http://"`oc get route backend -o template --template='{{ .spec.host }}'`"/q/swagger-ui"
+echo
+
